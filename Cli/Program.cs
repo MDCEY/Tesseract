133d32e5d40fcfc04eac1050ddbea6f0a60c5025
Kieran Wynne <3812864+MDCEY@users.noreply.github.com>
2019-12-27 16:35:13 +0000

    Add GUI

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using Workshop;
+using CommandLine;
 
 namespace Tesseract
 {
-    internal static class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        public class Options
         {
-            string engineer = args[0];
-            Console.WriteLine(Workshop.Team.RepairsToday());
-            Console.WriteLine(Workshop.Engineer.RepairsToday(engineer));
-            Console.WriteLine(Workshop.Engineer.RepairedWorkingTime(engineer));
+            
+        }
+
+        [Verb("repairs", HelpText = "Fetch your stats")]
+        public class RepairOptions
+        {
+            [Option('u', "User", Default = "all")]
+            public string User { get; set; }
+        }    
+        
+        public static int Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<RepairOptions>(args)
+                .MapResult(
+                    (RepairOptions opts) => GetRepairs(opts),
+                    errs => 1);
+        }
+
+        private static int GetRepairs(RepairOptions args)
+        {
+            Console.WriteLine($"Getting stats for {args.User}");
+            if (args.User == "all")
+            {
+                Console.WriteLine($"{Team.RepairsToday()} completed repairs");
+            }
+            else
+            {
+                Console.WriteLine($"{Workshop.Engineer.RepairsToday(args.User)} completed repairs");
+                Console.WriteLine($"{Workshop.Engineer.RepairedWorkingTime(args.User)} spent on repaired items");
+            }
+            return 0;
         }
     }
 }
---
+++ Gui/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using Workshop;
+namespace Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        private void GetRepairs_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (GetRepairs.IsChecked.Value)
+            {
+                TotalRepairs.Content = EngineerInput.Text.Length > 0
+                    ? $"{Workshop.Engineer.RepairsToday(EngineerInput.Text)} Repairs"
+                    : $"{Workshop.Team.RepairsToday()} Total Repairs";
+            }
+            else
+            {
+                TotalRepairs.Content = "";
+            }
+
+            if (GetTime.IsChecked.Value)
+            {
+                TotalTime.Content = EngineerInput.Text.Length > 0
+                    ? $"{Workshop.Engineer.RepairedWorkingTime(EngineerInput.Text)}"
+                    : "";
+            }
+            else
+            {
+                TotalTime.Content = "";
+            }
+
+        }
+    }
+}
