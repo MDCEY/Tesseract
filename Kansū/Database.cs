361bb65878256faef85c85b73dc887552d6c7d6e
Kieran Wynne <Keewynne90@gmail.com>
2020-03-05 22:21:55 +0000

    Add command generator. Add engineer days stats

--- Kansū/Database.cs
+++ Kansū/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,24 @@ namespace Kansū
 {
     internal class Database
     {
-        readonly SqlConnection Connection;
+        internal SqlConnection Connection;
 
         internal Database()
         {
             byte[] connectionString = Convert.FromBase64String(@"RGF0YSBTb3VyY2U9MTAuMTIxLjY4LjY2XENPT1BFU09MQlJBTkNITElWRSwxODM3OyBJbnRlZ3JhdGVkIFNlY3VyaXR5PUZhbHNlO1VzZXIgSUQ9dGVzc2VyYWN0O1Bhc3N3b3JkPVRlNTVlcmFjdA==");
             string decodedConnectionString = Encoding.UTF8.GetString(connectionString);
             Connection = new SqlConnection(decodedConnectionString);
+        }
+
+        internal SqlCommand CreateCommand(SqlConnection connection, string query)
+        {
+            SqlCommand command = new SqlCommand();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = query;
+            return command;
+
         }
     }
 }
--- Kansū/Workshop.cs
+++ Kansū/Workshop.cs
@@ -8,5 +8,25 @@ namespace Kansū
 {
     class Workshop
     {
+        public static int RepairsToday(string engineerNumber)
+        {
+            int total = 0;
+            const string query = "SELECT " +
+                                   "ISNULL(COUNT(Call_Num),0) as 'Repairs'" +
+                                   "FROM COOPESOLBRANCHLIVE.dbo.SCCall " +
+                                   "WHERE Job_CDate between Convert(DateTime, DATEDIFF(DAY, 0, GETDATE())) and Dateadd(day, 1, DATEDIFF(DAY, 0, GETDATE())) " +
+                                   "AND Call_Employ_Num = @User";
+
+            Database database = new Database();
+            var connection = database.Connection;
+            var command = database.CreateCommand(connection, query);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                total = reader.GetInt32(0);
+            }
+            return total;
+        }
+
     }
 }
