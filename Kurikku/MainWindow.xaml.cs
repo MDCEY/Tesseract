8e0faf8c1980005563dbdff245e5cb5cecaae084
Kieran Wynne <keewynne90@gmail.com>
2020-03-06 16:32:16 +0000

    WIP: Interface

--- Kurikku/MainWindow.xaml.cs
+++ Kurikku/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Kansū;
 
 namespace Kurikku
 {
@@ -23,6 +24,12 @@ namespace Kurikku
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var data = PartsCage.EngineerParts();
+
         }
     }
 }
