964bf01df9c37cfb6f36e03a9f6ec97b264ab8b0
Kieran Wynne <keewynne90@gmail.com>
2020-05-15 11:21:29 +0100

    Remove non-required references

--- Intāfēsu/MainWindow.xaml.cs
+++ Intāfēsu/MainWindow.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using MahApps.Metro.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Intāfēsu
 {
