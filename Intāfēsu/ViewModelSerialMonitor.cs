ebeb4f5c9693f5cf52e1758d4440b15f1d8ce9bc
Kieran Wynne <keewynne90@gmail.com>
2020-11-26 14:56:34 +0000

    barebones of serial monitor added

--- Intāfēsu/ViewModelSerialMonitor.cs
+++ Intāfēsu/ViewModelSerialMonitor.cs
@@ -18,5 +18,68 @@ namespace Intāfēsu
 {
     class ViewModelSerialMonitor : ViewModelBase
     {
+        private MonitoredSerialNumber _monitoredSerialNumber; //each collection line is this.
+        private ObservableCollection<MonitoredSerialNumber> _monitoredSerialNumbers; // This is the main coolection
+        private ICommand _addSerialCommand; // this controls the add button
+        private string _userSerialNumber; // text box serial number for user input
+        public MonitoredSerialNumber MonitoredSerialNumber
+        {
+            get => _monitoredSerialNumber;
+            set
+            {
+                _monitoredSerialNumber = value;
+                NotifyPropertyChanged(nameof(MonitoredSerialNumber));
+            }
+        }
+        public ObservableCollection<MonitoredSerialNumber> MonitoredSerialNumbers
+        {
+            get => _monitoredSerialNumbers;
+            set
+            {
+                _monitoredSerialNumbers = value;
+                NotifyPropertyChanged(nameof(MonitoredSerialNumbers));
+            }
+        }
+
+        public ICommand AddSerialCommand { get; set; }
+
+        public string UserSerialNumber
+        {
+            get => _userSerialNumber;
+            set
+            {
+                _userSerialNumber = value;
+            }
+        }
+
+        private bool CanExecuteAddSerialCommand(object parameter)
+        {
+            if (string.IsNullOrEmpty(UserSerialNumber))
+            {
+                return false;
+            }
+            return UserSerialNumber != "";
+        }
+
+        public ViewModelSerialMonitor()
+        {
+            AddSerialCommand = new RelayCommand(executeAddSerial ,CanExecuteAddSerialCommand);
+        }
+
+        private void executeAddSerial(object parameter)
+        {
+            MessageBox.Show("zZZZZ");
+        }
     }
+
+    public class MonitoredSerialNumber
+    {
+        public string SerialNumber { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string CallNumber { get; set; }
+        public string User { get; set; }
+
+    }
+
+
 }
